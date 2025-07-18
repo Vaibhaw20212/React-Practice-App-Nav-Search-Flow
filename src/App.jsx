1be4616dc3971a6e53000f74ab8a4368b005d73e
Bhaiya Vaibhaw Kumar <bhaiyavaibhaw@gmail.com>
2025-07-18 14:05:30 +0530

    hash router removed from App.jsx to avoid repetition.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Route, Switch, HashRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Playground from "./pages/Playground";
@@ -10,18 +10,18 @@ function App() {
   const showPlayground = useSelector((state) => state.nav.showPlayground);
   const showBitcoin = useSelector((state) => state.nav.showBitcoin);
   return (
-      <HashRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          {showPlayground ? (
-            <Route path="/playground" component={Playground} />
-          ) : null}
-          {showBitcoin ? (
-            <Route path="/bitcoin-chart" component={BitcoinChartPage} />
-          ) : null}
-        </Switch>
-      </HashRouter>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {showPlayground ? (
+          <Route path="/playground" component={Playground} />
+        ) : null}
+        {showBitcoin ? (
+          <Route path="/bitcoin-chart" component={BitcoinChartPage} />
+        ) : null}
+      </Switch>
+    </>
   );
 }
 
